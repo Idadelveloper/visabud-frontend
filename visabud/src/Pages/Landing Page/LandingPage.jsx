f4ffac7221dc31b14e042f431083c715abbbb59f
Fienne <ngumihfien@gmail.com>
2023-09-01 22:17:35 +0100

    made UI changes

--- visabud/src/Pages/Landing Page/LandingPage.jsx
+++ visabud/src/Pages/Landing Page/LandingPage.jsx
@@ -5,11 +5,19 @@ import { Container } from "react-bootstrap";
 const LandingPage = () => {
   return (
     <Container>
-      <div>
-        <h1>
-          Let us take you from <br /> Africa to the World
-        </h1>
-      </div>
+      <Row>
+        <Col>
+          <h1>
+            Let us take you from <br /> Africa to the World
+          </h1>
+          <p>
+            Allow us to assist you with touring around the world with ease and
+            comfort, providing you with all the details you need in getting into
+            any country of your choice!
+          </p>
+          
+        </Col>
+      </Row>
     </Container>
   );
 };
