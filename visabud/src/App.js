e8ab8eb861324f948aa5a9e2725eafa85b5270da
enjeck <patrathewhiz@gmail.com>
2023-10-08 14:14:05 +0100

    Add visa suggestion

--- visabud/src/App.js
+++ visabud/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import ChatPage from "./Pages/ChatPage/ChatPage"
 import ContextFormPage from "./Pages/Context/ContextFormPage";
+import VisaNotRequiredPage from "./Pages/VisaNotRequired/VisaNotRequiredPage";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/chat" element={<ChatPage />} />
+          <Route path="/visa-not-required" element={<VisaNotRequiredPage />} />
           <Route path="/start" element={<ContextFormPage />} />
        </Routes>
     </div>
